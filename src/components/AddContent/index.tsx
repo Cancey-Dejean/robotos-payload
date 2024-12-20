552b9bdff018ca03d30b74af315fccc0928fa91b
Cancey Dejean <canceyd@gmail.com>
2024-12-19 18:45:05 -0600

    committed starter files

---
+++ src/components/AddContent/index.tsx
@@ -0,0 +1,7 @@
+export default function AddContent() {
+  return (
+    <div className="min-h-[100sdvh] py-40 text-center px-5">
+      <h1 className="text-3xl font-bold dark:text-white text-black">Add sections in CMS.</h1>
+    </div>
+  )
+}
